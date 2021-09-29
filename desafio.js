c71a592c0fd5ea27a5063567f2de85f8620be909
Matheus Raimundo <61631379+Mathiore@users.noreply.github.com>
2021-09-29 20:06:29 -0300

    Add files via upload

---
+++ desafio.js
@@ -0,0 +1,33 @@
+function retornaValorCedula(cedula){
+    if(cedula == 100){
+        cedula = 50
+    } else if(cedula == 50){
+        cedula = 10
+    } else if(cedula == 10){
+        cedula = 5
+    } else if(cedula == 5){
+        cedula = 1
+    }
+    return cedula
+}
+
+function saqueCaixaEletronico(saque){
+    cedula = 100
+    contadorcedula = 0
+    while (saque >= 0) {
+        if (saque >= cedula){
+            saque = saque - cedula
+            contadorcedula += 1
+        } else{
+            if (contadorcedula > 0){
+                console.log(contadorcedula, `Cédula de ${cedula} reais`)
+            }
+            verificaCedula(cedula)
+            contadorcedula = 0
+            if (saque == 0){
+                break
+            }
+        }
+    }
+}
+
